2b744d15e0e5dbec1872f65b153db2223e3442d9
Zac Witte <zacwitte@gmail.com>
2018-04-01 12:19:56 -0700

    finished the partial updater

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -3,6 +3,7 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
+from std_msgs.msg import Int32
 
 import math
 
@@ -21,7 +22,7 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+LOOKAHEAD_WPS = 200  # Number of waypoints we will publish. You can change this number
 
 
 class WaypointUpdater(object):
@@ -31,30 +32,61 @@ class WaypointUpdater(object):
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
-        # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
-
+        rospy.Subscriber('/obstacle_waypoint', Int32, self.obstacle_cb)
+        rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
-        # TODO: Add other member variables you need below
+        self.waypoints = None
+        self.current_pose = None
+        self.current_waypoint_id = 0
 
         rospy.spin()
 
     def pose_cb(self, msg):
-        # TODO: Implement
-        pass
+        """
+        ROS callback for the car's current pose in the /world coordinate frame. Updated at 50Hz.
+        We update the
+        """
+        self.current_pose = msg
+        if self.waypoints:
+            self.current_waypoint_id = self.find_next_closest_waypoint(self.current_pose)
+            self.publish_next_waypoints(self.current_waypoint_id, msg.header.stamp)
 
     def waypoints_cb(self, waypoints):
-        # TODO: Implement
-        pass
+        """
+        ROS callback for a waypoint list which is fired once at startup from a latched topic.
+
+        :param waypoints: the list of waypoints in the /world frame
+        :type  waypoints: styx_msgs.msg.Lane
+        """
+        rospy.loginfo("waypoints %s", len(waypoints.waypoints))
+        self.waypoints = waypoints.waypoints
 
     def traffic_cb(self, msg):
+        """
+        ROS callback for detected traffic lights. This returns -1 when the camera is turned on in
+        the simulator.
+
+        TODO: fill out more details once I understand it more.
+
+        :param msg: traffic light waypoint id
+        :type  msg: std_msgs.msg.Int32
+        """
         # TODO: Callback for /traffic_waypoint message. Implement
-        pass
+        rospy.loginfo("traffic wp: %s", msg)
 
     def obstacle_cb(self, msg):
+        """
+        ROS callback for detected obstacles.
+
+        TODO: fill out more details once I understand it more.
+
+        :param msg: obstacle waypoint id
+        :type  msg: std_msgs.msg.Int32
+        """
         # TODO: Callback for /obstacle_waypoint message. We will implement it later
-        pass
+        rospy.loginfo("obstacle wp: %s", msg)
 
     def get_waypoint_velocity(self, waypoint):
         return waypoint.twist.twist.linear.x
@@ -62,14 +94,42 @@ class WaypointUpdater(object):
     def set_waypoint_velocity(self, waypoints, waypoint, velocity):
         waypoints[waypoint].twist.twist.linear.x = velocity
 
+    def find_next_closest_waypoint(self, pose):
+        assert self.waypoints, "Waypoint list is blank"
+        closest = 9999999
+        closest_id = 0
+        # I assume the car is only going forward in the waypoint list
+        start_id = max(0, self.current_waypoint_id - 10)
+        for i in xrange(start_id, len(self.waypoints)):
+            dist = dl(pose.pose.position, self.waypoints[i].pose.pose.position)
+            if dist < closest:
+                closest_id = i
+                closest = dist
+            elif self.current_waypoint_id > 0:
+                # This means we're getting further away from the closest waypoint. Except if we're
+                # starting from the beginning we can't assume the local minima is the closest.
+                break
+
+        return closest_id
+
+    def publish_next_waypoints(self, start_id, stamp):
+        msg = Lane()
+        msg.header.frame_id = '/world'
+        msg.header.stamp = stamp
+        msg.waypoints = self.waypoints[start_id + 1 : start_id + 1 + LOOKAHEAD_WPS]
+        rospy.loginfo("Published final_waypoints starting at %s", start_id)
+        self.final_waypoints_pub.publish(msg)
+
     def distance(self, waypoints, wp1, wp2):
         dist = 0
-        dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
-        for i in range(wp1, wp2+1):
+        # dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
+        for i in range(wp1, wp2 + 1):
             dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
             wp1 = i
         return dist
 
+def dl(pos1, pos2):
+    return math.sqrt((pos1.x - pos2.x)**2 + (pos1.y - pos2.y)**2 + (pos1.z - pos2.z)**2)
 
 if __name__ == '__main__':
     try:
