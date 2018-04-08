d2b1bbaecb40c33438a44e8fee977d88a516c068
Gavin Ong <gavinong10@gmail.com>
2018-04-08 07:40:41 -0700

    Remove debug logging from waypoint_loader

--- ros/src/waypoint_loader/waypoint_loader.py
+++ ros/src/waypoint_loader/waypoint_loader.py
@@ -18,7 +18,7 @@ MAX_DECEL = 1.0
 class WaypointLoader(object):
 
     def __init__(self):
-        rospy.init_node('waypoint_loader', log_level=rospy.DEBUG)
+        rospy.init_node('waypoint_loader')
 
         self.pub = rospy.Publisher('/base_waypoints', Lane, queue_size=1, latch=True)
 
