7d0c5a21ceebe40f82d130d30c3b7ee7f3b37ff1
Gavin Ong <gavinong10@gmail.com>
2018-04-14 15:53:53 -0700

    traffic light object detector completed

--- ros/src/tl_detector/modelling/fetch_data.sh
+++ ros/src/tl_detector/modelling/fetch_data.sh
@@ -4,12 +4,19 @@ echo "Please make sure that you have the correct python environment installed an
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-aws s3 sync s3://udacity-kitt-training-datasets ./$SCRIPTPATH/data
+# aws s3 sync s3://udacity-kitt-training-datasets ./$SCRIPTPATH/data
 
-wget http://download.tensorflow.org/models/object_detection/faster_rcnn_resnet101_kitti_2018_01_28.tar.gz $SCRIPTPATH/faster_rcnn_resnet101_kitti_2018_01_28.tar.gz
-tar xzvf $SCRIPTPATH/faster_rcnn_resnet101_kitti_2018_01_28.tar.gz .
+#wget http://download.tensorflow.org/models/object_detection/faster_rcnn_inception_v2_coco_2018_01_28.tar.gz $SCRIPTPATH/faster_rcnn_inception_v2_coco_2018_01_28.tar.gz
+#tar xzvf $SCRIPTPATH/faster_rcnn_inception_v2_coco_2018_01_28.tar.gz .
 
-cd $SCRIPTPATH/cocoapi/PythonAPI
-make
+#cd $SCRIPTPATH/cocoapi/PythonAPI
+#make
 
-# tensorflow_path=$(pip show tensorflow | grep Location | cut -d' ' -f 2)
+site_packages_path=$(pip show tensorflow | grep Location | cut -d' ' -f 2)
+
+# Need to follow https://medium.com/@WuStangDan/step-by-step-tensorflow-object-detection-api-tutorial-part-1-selecting-a-model-a02b6aabe39e
+#cd $SCRIPTPATH/tensorflow/research/
+#protoc object_detection/protos/*.proto --python_out=.
+
+#echo $SCRIPTPATH/tensorflow/research/slim > $site_packages_path/slim.pth
+#echo $SCRIPTPATH/tensorflow/research > $site_packages_path/research.pth
