f7b1aed228004e8ed756c8e196ecb01467280e5f
Gavin Ong <gavinong10@gmail.com>
2018-04-21 12:24:46 -0700

    Run export reference graph script

---
+++ ros/src/tl_detector/modelling/object_detection/final/run_export_reference_graph.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python export_inference_graph.py --input_type image_tensor --pipeline_config_path ./models/train/faster_rcnn_inception_v2/pipeline.config --trained_checkpoint_prefix ./models/train/faster_rcnn_inception_v2/model.ckpt-200 --output_directory ./models/faster_rcnn_inception_v2_bosch
+python export_inference_graph.py --input_type image_tensor --pipeline_config_path ./models/train/ssd_mobilenet_v2/pipeline.config --trained_checkpoint_prefix ./models/train/ssd_mobilenet_v2/model.ckpt-200 --output_directory ./models/ssd_mobilenet_v2_bosch
