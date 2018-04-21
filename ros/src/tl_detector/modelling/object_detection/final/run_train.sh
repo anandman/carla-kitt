9509eb41559120d8b0cc7ad9d19138b2cf8579b4
Gavin Ong <gavinong10@gmail.com>
2018-04-20 20:56:22 -0700

    Add mobilenet training

--- ros/src/tl_detector/modelling/object_detection/final/run_train.sh
+++ ros/src/tl_detector/modelling/object_detection/final/run_train.sh
@@ -5,3 +5,5 @@ DIRNAME=$(dirname "$0")
 cd ${DIRNAME}
 
 python train.py --logtostderr --train_dir=./models/train --pipeline_config_path=./ssd_inception_v2_coco.config
+
+python train.py --logtostderr --train_dir=./models/train --pipeline_config_path=./ssd_mobilenet_v2_coco.config
