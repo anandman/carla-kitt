fdd2e03dae3ee2a9431b6c4c0324d77ebc464418
Gavin Ong <gavinong10@gmail.com>
2018-04-21 11:21:52 -0700

    Rename configs and some paths

--- ros/src/tl_detector/modelling/object_detection/final/run_train.sh
+++ ros/src/tl_detector/modelling/object_detection/final/run_train.sh
@@ -4,6 +4,6 @@ DIRNAME=$(dirname "$0")
 
 cd ${DIRNAME}
 
-python train.py --logtostderr --train_dir=./models/train --pipeline_config_path=./faster_rcnn_inception_v2_coco.config
+python train.py --logtostderr --train_dir=./models/train/faster_rcnn_inception_v2 --pipeline_config_path=./faster_rcnn_inception_v2_bosch.config
 
-python train.py --logtostderr --train_dir=./models/train --pipeline_config_path=./ssd_mobilenet_v2_coco.config
+python train.py --logtostderr --train_dir=./models/train/ssd_mobilenet_v2 --pipeline_config_path=./ssd_mobilenet_v2_bosch.config
